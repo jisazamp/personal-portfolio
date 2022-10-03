4077a6989d0e31f929e5ccf2c8ca17be63535611
Juan P. Isaza <jisazamp@gmail.com>
2022-10-03 10:58:13 -0500

    Optimized images for mobile

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,7 +1,6 @@
 import react from '../assets/img/react.svg'
 import javascript from '../assets/img/javascript.svg'
 import typescript from '../assets/img/typescript.svg'
-import python from '../assets/img/python.svg'
 import django from '../assets/img/django.svg'
 import nodejs from '../assets/img/nodejs.svg'
 
@@ -21,7 +20,6 @@ export const skills: Skill[] = [
     text: 'JavaScript',
   },
   { id: 3, img: typescript, level: 'Medium', text: 'TypeScript' },
-  { id: 4, img: python, level: 'Medium/Advanced', text: 'Python' },
-  { id: 5, img: django, level: 'Medium', text: 'Django' },
-  { id: 6, img: nodejs, level: 'Medium', text: 'NodeJS' },
+  { id: 4, img: django, level: 'Medium', text: 'Django' },
+  { id: 5, img: nodejs, level: 'Medium', text: 'NodeJS' },
 ]
